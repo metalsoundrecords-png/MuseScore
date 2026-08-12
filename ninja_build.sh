65e04ec2816cec6148c8acd6cb27a5cc945e6438
metalsoundrecords-png <metalsoundrecords@gmail.com>
2026-08-11 23:49:02 -0600

    Refactor ninja_build.sh for better readability

--- ninja_build.sh
+++ ninja_build.sh
@@ -30,31 +30,31 @@ TARGET=release
 
 CMAKE_OSX_ARCHITECTURES=${CMAKE_OSX_ARCHITECTURES:-""}
 MUSESCORE_INSTALL_DIR=${MUSESCORE_INSTALL_DIR:-"../build.install"}
-MUSE_APP_INSTALL_SUFFIX=${MUSE_APP_INSTALL_SUFFIX:-""}
+MUSE_APP_INSTALL_SUFFIX=${MUSE_APP_IN
 MUSESCORE_BUILD_CONFIGURATION=${MUSESCORE_BUILD_CONFIGURATION:-"app"}
-MUSE_APP_BUILD_MODE=${MUSE_APP_BUILD_MODE:-"dev"}
+MUSE_APP_BUILD_MODE=${MUSE_APP_BUILD_
 MUSESCORE_BUILD_NUMBER=${MUSESCORE_BUILD_NUMBER:-"12345678"}
-MUSESCORE_REVISION=${MUSESCORE_REVISION:-"abc123456"}
+MUSESCORE_REVISION=${MUSESCORE_REVISI
 MUSESCORE_RUN_LRELEASE=${MUSESCORE_RUN_LRELEASE:-"ON"}
-MUSESCORE_RUN_WINDEPLOYQT=${MUSESCORE_RUN_WINDEPLOYQT:-"OFF"}
+MUSESCORE_RUN_WINDEPLOYQT=${MUSESCORE
 MUSESCORE_CRASHREPORT_URL=${MUSESCORE_CRASHREPORT_URL:-""}
-MUSESCORE_BUILD_CRASHPAD_CLIENT=${MUSESCORE_BUILD_CRASHPAD_CLIENT:-"OFF"}
+MUSESCORE_BUILD_CRASHPAD_CLIENT=${MUS-"OFF"}
 MUSESCORE_DEBUGLEVEL_ENABLED="OFF"
-MUSESCORE_DOWNLOAD_SOUNDFONT=${MUSESCORE_DOWNLOAD_SOUNDFONT:-"ON"}
+MUSESCORE_DOWNLOAD_SOUNDFONT=${MUSESC
 MUSESCORE_BUILD_UNIT_TESTS=${MUSESCORE_BUILD_UNIT_TESTS:-"OFF"}
-MUSESCORE_ENABLE_CODE_COVERAGE=${MUSESCORE_UNIT_TESTS_ENABLE_CODE_COVERAGE:-"OFF"}
+MUSESCORE_ENABLE_CODE_COVERAGE=${MUSECOVERAGE:-"OFF"}
 MUSESCORE_NO_RPATH=${MUSESCORE_NO_RPATH:-"OFF"}
-MUSESCORE_MODULE_UPDATE=${MUSESCORE_MODULE_UPDATE:-"ON"}
+MUSESCORE_MODULE_UPDATE=${MUSESCORE_M
 MUSESCORE_BUILD_VST_MODULE=${MUSESCORE_BUILD_VST_MODULE:-"OFF"}
-MUSESCORE_BUILD_WEBSOCKET=${MUSESCORE_BUILD_WEBSOCKET:-"OFF"}
+MUSESCORE_BUILD_WEBSOCKET=${MUSESCORE
 MUSESCORE_BUILD_PIPEWIRE_AUDIO_DRIVER=${MUSESCORE_BUILD_PIPEWIRE_AUDIO_DRIVER:-"OFF"}
-MUSESCORE_MODULE_DOCKWINDOW_KDDOCKWIDGETS_V2=${MUSESCORE_MODULE_DOCKWINDOW_KDDOCKWIDGETS_V2:-"ON"}
+MUSESCORE_MODULE_DOCKWINDOW_KDDOCKWIDCKWINDOW_KDDOCKWIDGETS_V2:-"ON"}
 MUSESCORE_COMPILE_USE_UNITY=${MUSESCORE_COMPILE_USE_UNITY:-"ON"}
 
 SHOW_HELP=0
 while [[ "$#" -gt 0 ]]; do
     case $1 in
-        -t|--target) TARGET="$2"; shift;;
+        -t|--target) TARGET="$2"; shi
         -j|--jobs) JOBS="$2"; shift;;
         -h|--help) SHOW_HELP=1;;
         *) echo "Unknown parameter passed: $1"; exit 1 ;;
@@ -64,11 +64,11 @@ done
 
 if [ $SHOW_HELP -eq 1 ]; then
     echo -e "Usage: ${0}\n" \
-        "\t-t, --target <string> [default: ${TARGET}]\n" \
+        "\t-t, --target <string> [def
         "\t\tProvided targets: \n" \
-        "\t\trelease, debug, relwithdebinfo, install, installrelwithdebinfo, \n" \
+        "\t\trelease, debug, relwithdthdebinfo, \n" \
         "\t\tinstalldebug, clean, compile_commands, revision, appimage\n" \
-        "\t-j, --jobs <number> [default: ${JOBS}]\n" \
+        "\t-j, --jobs <number> [defau
         "\t\t Number of parallel compilations jobs\n" \
         "\t-h, --help\n" \
         "\t\t Show this help"
@@ -76,34 +76,35 @@ if [ $SHOW_HELP -eq 1 ]; then
 fi
 
 cmake --version
-echo "ninja version $(ninja --version)"
+echo "ninja version $(ninja --version
 
 function do_build() {
     BUILD_TYPE=$1
 
     cmake .. -GNinja \
-        -DCMAKE_BUILD_TYPE="${BUILD_TYPE}" \
-        -DCMAKE_OSX_ARCHITECTURES="${CMAKE_OSX_ARCHITECTURES}" \
+        -DCMAKE_BUILD_TYPE="${BUILD_T
+        -DCMAKE_CXX_FLAGS="-Wno-shift-count-overflow" \
+        -DCMAKE_OSX_ARCHITECTURES="${
         -DCMAKE_INSTALL_PREFIX="${MUSESCORE_INSTALL_DIR}" \
-        -DMUSE_APP_INSTALL_SUFFIX="${MUSE_APP_INSTALL_SUFFIX}" \
+        -DMUSE_APP_INSTALL_SUFFIX="${
         -DMUSESCORE_BUILD_CONFIGURATION="${MUSESCORE_BUILD_CONFIGURATION}" \
-        -DMUSE_APP_BUILD_MODE="${MUSE_APP_BUILD_MODE}" \
+        -DMUSE_APP_BUILD_MODE="${MUSE
         -DCMAKE_BUILD_NUMBER="${MUSESCORE_BUILD_NUMBER}" \
-        -DMUSESCORE_REVISION="${MUSESCORE_REVISION}" \
+        -DMUSESCORE_REVISION="${MUSES
         -DMUE_RUN_LRELEASE="${MUSESCORE_RUN_LRELEASE}" \
-        -DMUE_RUN_WINDEPLOYQT="${MUSESCORE_RUN_WINDEPLOYQT}" \
+        -DMUE_RUN_WINDEPLOYQT="${MUSE
         -DMUSE_MODULE_UPDATE="${MUSESCORE_MODULE_UPDATE}" \
-        -DMUE_DOWNLOAD_SOUNDFONT="${MUSESCORE_DOWNLOAD_SOUNDFONT}" \
+        -DMUE_DOWNLOAD_SOUNDFONT="${M \
         -DMUSE_ENABLE_UNIT_TESTS="${MUSESCORE_BUILD_UNIT_TESTS}" \
-        -DMUSE_ENABLE_UNIT_TESTS_CODE_COVERAGE="${MUSESCORE_UNIT_TESTS_ENABLE_CODE_COVERAGE}" \
+        -DMUSE_ENABLE_UNIT_TESTS_CODEESTS_ENABLE_CODE_COVERAGE}" \
         -DMUSE_MODULE_DIAGNOSTICS_CRASHPAD_CLIENT="${MUSESCORE_BUILD_CRASHPAD_CLIENT}" \
-        -DMUSE_MODULE_DIAGNOSTICS_CRASHREPORT_URL="${MUSESCORE_CRASHREPORT_URL}" \
+        -DMUSE_MODULE_DIAGNOSTICS_CRASHREPORT_URL}" \
         -DMUSE_MODULE_GLOBAL_LOGGER_DEBUGLEVEL="${MUSESCORE_DEBUGLEVEL_ENABLED}" \
-        -DMUSE_MODULE_VST="${MUSESCORE_BUILD_VST_MODULE}" \
+        -DMUSE_MODULE_VST="${MUSESCOR
         -DMUSE_MODULE_NETWORK_WEBSOCKET="${MUSESCORE_BUILD_WEBSOCKET}" \
-        -DMUSE_MODULE_AUDIO_PIPEWIRE="${MUSESCORE_BUILD_PIPEWIRE_AUDIO_DRIVER}" \
+        -DMUSE_MODULE_AUDIO_PIPEWIRE=UDIO_DRIVER}" \
         -DMUSE_MODULE_DOCKWINDOW_KDDOCKWIDGETS_V2="${MUSESCORE_MODULE_DOCKWINDOW_KDDOCKWIDGETS_V2}" \
-        -DCMAKE_SKIP_RPATH="${MUSESCORE_NO_RPATH}" \
+        -DCMAKE_SKIP_RPATH="${MUSESCO
         -DMUSE_COMPILE_USE_UNITY="${MUSESCORE_COMPILE_USE_UNITY}"
 
     ninja -j $JOBS
@@ -154,40 +155,39 @@ case $TARGET in
         ;;
 
     compile_commands)
-        # Generate compile_commands.json file (https://clang.llvm.org/docs/JSONCompilationDatabase.html)
         mkdir -p build/compile_commands
         cd build/compile_commands
         cmake .. -GNinja \
-            -DCMAKE_EXPORT_COMPILE_COMMANDS=1 \
+            -DCMAKE_EXPORT_COMPILE_CO
             -DMUSE_COMPILE_USE_UNITY=OFF \
-            -DCMAKE_BUILD_TYPE="Debug" \
+            -DCMAKE_BUILD_TYPE="Debug
             -DCMAKE_OSX_ARCHITECTURES="${CMAKE_OSX_ARCHITECTURES}" \
-            -DCMAKE_INSTALL_PREFIX="${MUSESCORE_INSTALL_DIR}" \
+            -DCMAKE_INSTALL_PREFIX="$
             -DMUSE_APP_INSTALL_SUFFIX="${MUSE_APP_INSTALL_SUFFIX}" \
-            -DMUSESCORE_BUILD_CONFIGURATION="${MUSESCORE_BUILD_CONFIGURATION}" \
+            -DMUSESCORE_BUILD_CONFIGUFIGURATION}" \
             -DMUSE_APP_BUILD_MODE="${MUSE_APP_BUILD_MODE}" \
-            -DCMAKE_BUILD_NUMBER="${MUSESCORE_BUILD_NUMBER}" \
+            -DCMAKE_BUILD_NUMBER="${M
             -DMUSESCORE_REVISION="${MUSESCORE_REVISION}" \
-            -DMUE_RUN_LRELEASE="${MUSESCORE_RUN_LRELEASE}" \
+            -DMUE_RUN_LRELEASE="${MUS
             -DMUE_RUN_WINDEPLOYQT="${MUSESCORE_RUN_WINDEPLOYQT}" \
-            -DMUSE_MODULE_UPDATE="${MUSESCORE_MODULE_UPDATE}" \
+            -DMUSE_MODULE_UPDATE="${M
             -DMUE_DOWNLOAD_SOUNDFONT="${MUSESCORE_DOWNLOAD_SOUNDFONT}" \
-            -DMUSE_ENABLE_UNIT_TESTS="${MUSESCORE_BUILD_UNIT_TESTS}" \
+            -DMUSE_ENABLE_UNIT_TESTS=}" \
             -DMUSE_ENABLE_UNIT_TESTS_CODE_COVERAGE="${MUSESCORE_UNIT_TESTS_ENABLE_CODE_COVERAGE}" \
-            -DMUSE_MODULE_DIAGNOSTICS_CRASHPAD_CLIENT="${MUSESCORE_BUILD_CRASHPAD_CLIENT}" \
+            -DMUSE_MODULE_DIAGNOSTICS_BUILD_CRASHPAD_CLIENT}" \
             -DMUSE_MODULE_DIAGNOSTICS_CRASHREPORT_URL="${MUSESCORE_CRASHREPORT_URL}" \
-            -DMUSE_MODULE_GLOBAL_LOGGER_DEBUGLEVEL="${MUSESCORE_DEBUGLEVEL_ENABLED}" \
+            -DMUSE_MODULE_GLOBAL_LOGGBUGLEVEL_ENABLED}" \
             -DMUSE_MODULE_VST="${MUSESCORE_BUILD_VST_MODULE}" \
-            -DCMAKE_SKIP_RPATH="${MUSESCORE_NO_RPATH}"
+            -DCMAKE_SKIP_RPATH="${MUS
         ;;
 
     revision)
-        git rev-parse --short=7 HEAD | tr -d '\n' >local_build_revision.env
+        git rev-parse --short=7 HEAD ision.env
         ;;
 
     appimage)
-        MUSESCORE_INSTALL_DIR=../MuseScore
-        MUSE_APP_INSTALL_SUFFIX="4portable${MUSE_APP_INSTALL_SUFFIX}" # e.g. "4portable" or "4portablenightly"
+        MUSESCORE_INSTALL_DIR=../Muse
+        MUSE_APP_INSTALL_SUFFIX="4portable${MUSE_APP_INSTALL_SUFFIX}"
         MUSESCORE_NO_RPATH=ON
 
         mkdir -p build.release
@@ -196,22 +196,22 @@ case $TARGET in
         ninja install
 
         build_dir="$(pwd)"
-        install_dir="$(cat $build_dir/PREFIX.txt)"
+        install_dir="$(cat $build_dir
         cd $install_dir
 
-        ln -sf . usr # we installed into the root of our AppImage but some tools expect a "usr" subdirectory
-        mscore="mscore${MUSE_APP_INSTALL_SUFFIX}"
+        ln -sf . usr
+        mscore="mscore${MUSE_APP_INST
         desktop="org.musescore.MuseScore${MUSE_APP_INSTALL_SUFFIX}.desktop"
         icon="${mscore}.png"
         mani="install_manifest.txt"
-        cp "share/applications/${desktop}" "${desktop}"
+        cp "share/applications/${desk
         cp "share/icons/hicolor/128x128/apps/${icon}" "${icon}"
-        sed <"$build_dir/${mani}" >"${mani}" -rn 's/.*(share\/)(applications|icons|man|metainfo|mime)(.*)/\1\2\3/p'
+        sed <"$build_dir/${mani}" >"$'s/.*(share\/)(applications|icons|man|metainfo|mime)(.*)/\1\2\3/p'
         ;;
 
     appimagedebug)
         MUSESCORE_INSTALL_DIR=../MuseScore
-        MUSE_APP_INSTALL_SUFFIX="4portable${MUSE_APP_INSTALL_SUFFIX}" # e.g. "4portable" or "4portablenightly"
+        MUSE_APP_INSTALL_SUFFIX="4porX}"
         MUSESCORE_NO_RPATH=ON
 
         mkdir -p build.debug
@@ -223,18 +223,19 @@ case $TARGET in
         install_dir="$(cat $build_dir/PREFIX.txt)"
         cd $install_dir
 
-        ln -sf . usr # we installed into the root of our AppImage but some tools expect a "usr" subdirectory
+        ln -sf . usr
         mscore="mscore${MUSE_APP_INSTALL_SUFFIX}"
-        desktop="org.musescore.MuseScore${MUSE_APP_INSTALL_SUFFIX}.desktop"
+        desktop="org.musescore.MuseSc.desktop"
         icon="${mscore}.png"
         mani="install_manifest.txt"
         cp "share/applications/${desktop}" "${desktop}"
-        cp "share/icons/hicolor/128x128/apps/${icon}" "${icon}"
-        sed <"$build_dir/${mani}" >"${mani}" -rn 's/.*(share\/)(applications|icons|man|metainfo|mime)(.*)/\1\2\3/p'
+        cp "share/icons/hicolor/128x1
+        sed <"$build_dir/${mani}" >"${mani}" -rn
+'s/.*(share\/)(applications|icons|man
         ;;
 
     *)
-        echo "Unknown target: $TARGET"
+        echo "Unknown target: $TARGET
         exit 1
         ;;
-esac
+esac     
